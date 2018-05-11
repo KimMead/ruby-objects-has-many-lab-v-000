7061846eea70e09f42024d54c21684bb0ff8a8c1
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-05-11 20:19:02 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,9 +18,10 @@ class Author
     @@post_count +=1
   end
 
-    def add_post_by_title(title)
-      post = Post.new(title)
-       self.posts << post
-       post.author = self
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.posts << post
+    post.author = self
+    @@post_count +=1
     end
-  end 
+  end
